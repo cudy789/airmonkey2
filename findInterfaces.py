a3e58f2ef2a21c77ba41b6dee175c4df3e64fe27
Corey <corey.knutson789@gmail.com>
2019-04-21 00:48:27 -0500

    First attempt to make it json friendly

--- findInterfaces.py
+++ findInterfaces.py
@@ -15,20 +15,20 @@ try:
             if i[i.find("wlan")+5] == ":":
                 availableInterfaces.append(i[0:5])
                 # print("appending " + i[0:5])
-
+    print("[")
     for i in availableInterfaces:
         iwconfig = ""
         try:
             iwconfig = (check_output(["/sbin/iwconfig", i]).decode("utf-8"))
         except:
-            print("iwconfig error")
+#             print("iwconfig error")
 
         if (iwconfig.decode("utf-8").find("ESSID:")) >= 0:
             interfaceStatus = iwconfig.decode("utf-8").split("ESSID:")[1].split(" ")[0]
             if  interfaceStatus!= "off/any":
-                print(i + ": " + interfaceStatus.split("\"")[1])
+                print("{\"interface\": \"" + i + "\",\"connection\": \"" + interfaceStatus.split("\"")[1] + "\"}")
             else:
 
-                print(i + ": " + "not connected")
+                print("{\"interface\": \"" + i + "\",\"connection\": \"not connected\"}")
 except:
     print("error running findInterfaces.py...")
